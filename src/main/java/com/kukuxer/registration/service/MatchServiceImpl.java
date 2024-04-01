28ee0d62a587c44ff2bc3b4379382a4386fc104a
Yevhenii <999.haha.990@gmail.com>
2024-04-01 16:05:56 +0100

    +new confidence logic

--- src/main/java/com/kukuxer/registration/service/MatchServiceImpl.java
+++ src/main/java/com/kukuxer/registration/service/MatchServiceImpl.java
@@ -167,25 +167,30 @@ public class MatchServiceImpl implements MatchService {
                 userStatistic.getRating(),
                 loserStatistic.getRating(),
                 1,
-                updateConfidence(userStatistic.getTotalGamesPlayed(), userStatistic.getConfidence())
-        );
-
+                userStatistic.getConfidence());
 
         // Update loser's rating
         int loserRating = calculateNewRating(
                 loserStatistic.getRating(),
                 userStatistic.getRating(),
                 0,
-                updateConfidence(loserStatistic.getTotalGamesPlayed(), loserStatistic.getConfidence())
-        );
+                loserStatistic.getConfidence());
 
         // Save updated statistics
         userStatistic.setRating(winnerRating);
-        userStatistic.setConfidence(updateConfidence(userStatistic.getTotalGamesPlayed(), userStatistic.getConfidence()));
+        userStatistic.setConfidence(
+                updateConfidence(userStatistic.getTotalGamesPlayed(),
+                        userStatistic.getRating()
+                )
+        );
         userStatisticRepository.save(userStatistic);
 
         loserStatistic.setRating(loserRating);
-        loserStatistic.setConfidence(updateConfidence(loserStatistic.getTotalGamesPlayed(), loserStatistic.getConfidence()));
+        loserStatistic.setConfidence(
+                updateConfidence(loserStatistic.getTotalGamesPlayed(),
+                        loserStatistic.getRating()
+                )
+        );
         userStatisticRepository.save(loserStatistic);
     }
 
@@ -204,17 +209,15 @@ public class MatchServiceImpl implements MatchService {
         firstPlayerStatistic.setTotalGamesPlayed(firstPlayerStatistic.getTotalGamesPlayed() + 1);
         firstPlayerStatistic.setDraws(firstPlayerStatistic.getDraws() + 1);
         firstPlayerStatistic.setRating(calculateNewRating(
-                firstPlayerStatistic.getRating(),
-                firstPlayerStatistic.getRating(),
-                0.5,
-                updateConfidence(
-                        firstPlayerStatistic.getTotalGamesPlayed(),
-                        firstPlayerStatistic.getConfidence())
+                        firstPlayerStatistic.getRating(),
+                        firstPlayerStatistic.getRating(),
+                        0.5,
+                firstPlayerStatistic.getConfidence()
                 )
         );
         firstPlayerStatistic.setConfidence(updateConfidence(
                 firstPlayerStatistic.getTotalGamesPlayed(),
-                firstPlayerStatistic.getConfidence())
+                firstPlayerStatistic.getRating())
         );
         userStatisticRepository.save(firstPlayerStatistic);
 
@@ -225,16 +228,15 @@ public class MatchServiceImpl implements MatchService {
         secondPlayerStatistic.setTotalGamesPlayed(secondPlayerStatistic.getTotalGamesPlayed() + 1);
         secondPlayerStatistic.setDraws(secondPlayerStatistic.getDraws() + 1);
         secondPlayerStatistic.setRating(calculateNewRating(
-                secondPlayerStatistic.getRating(),
-                secondPlayerStatistic.getRating(),
+                        secondPlayerStatistic.getRating(),
+                        secondPlayerStatistic.getRating(),
                         0.5,
-                        updateConfidence(secondPlayerStatistic.getTotalGamesPlayed(),
-                                secondPlayerStatistic.getConfidence())
+                secondPlayerStatistic.getConfidence()
                 )
         );
         secondPlayerStatistic.setConfidence(updateConfidence(
                 secondPlayerStatistic.getTotalGamesPlayed(),
-                secondPlayerStatistic.getConfidence())
+                secondPlayerStatistic.getRating())
         );
         userStatisticRepository.save(secondPlayerStatistic);
     }
@@ -323,19 +325,34 @@ public class MatchServiceImpl implements MatchService {
     //If the player loses the game, the score would be 0.
     public int calculateNewRating(int playerRating, int opponentRating, double score, double confidence) {
         double expectedScore = calculateExpectedScore(playerRating, opponentRating);
-        double ratingChange = confidence * (score - expectedScore);
-        // Apply a floor and ceiling to rating change to prevent large swings
-        ratingChange = Math.max(-100, Math.min(100, ratingChange));
-        return playerRating + (int) ratingChange;
+        int ratingChange = (int) Math.ceil(confidence * (score - expectedScore));
+        return playerRating + ratingChange;
     }
 
     // Adjust the K-factor as needed based on your requirements
 
     private double calculateExpectedScore(int playerRating, int opponentRating) {
-        return 1.0 / (1.0 + Math.pow(10.0, (opponentRating - playerRating) / 400.0));
+        return (1.0 / (1.0 + Math.pow(10.0, (opponentRating - playerRating) / 400.0)));
     }
 
-    public double  updateConfidence(int totalGamesPlayed, double currentConfidence) {
-        return currentConfidence / (totalGamesPlayed + 1);
+    public double updateConfidence(int totalGamesPlayed, int rating) {
+        int k;
+
+        if (totalGamesPlayed < 30) {
+            k = 115 - totalGamesPlayed;
+        } else {
+            if (rating >= 2400) {
+                k = 20;
+            } else if (rating >= 2000) {
+                k = 30;
+            } else if (rating >= 1500) {
+                k = 40;
+            } else if (rating >= 500) {
+                k = 60;
+            } else {
+                k = 70;
+            }
+        }
+        return k;
     }
 }
